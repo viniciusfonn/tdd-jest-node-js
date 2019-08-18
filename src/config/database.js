6e97f3f1a8a076bec49d8535ff4710bf773ea664
vinicius fonseca <viniciusfon96@gmail.com>
2019-08-18 20:19:07 -0300

    created project structure

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'testesjest',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  loggin: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  },
+
+};
